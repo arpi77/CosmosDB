d603993349c32e2512bd10d9058bce6a3a620f2e
Arpad Kucserak <kucserak@gmail.com>
2019-08-15 19:12:15 +0200

    Add project files.

---
+++ CosmosDB/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+
+namespace CosmosDB
+{
+    class Program
+    {
+        private static string endpointUrl = "https://cosmos-test-dx.documents.azure.com:443/";
+
+        private static string authorizationKey =
+            "m1tZOlNMlXdlXqFuUkZPIJJXvsqrrgGJPnF1odhoQ9lT1GTttkjD70lqcvgUtBedZkzYRimriEh2A3MYDAivZQ==";
+
+        private static DocumentClient client;
+
+        static void Main(string[] args)
+        {
+            using (client = new DocumentClient(new Uri(endpointUrl), authorizationKey))
+            {
+               // client.CreateDatabaseIfNotExistsAsync(new Database {Id = "Supi1"}).Wait();
+                
+            }
+        }
+    }
+}
